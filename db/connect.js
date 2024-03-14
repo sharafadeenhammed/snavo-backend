65703c4b2949492a9c6a747bf34f1f06ff165667
Sharafadeen Hammed <sharafadeenhammed@yahoo.com>
2024-03-14 05:47:14 +0100

    ready for staging

--- db/connect.js
+++ db/connect.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 async function connectDb() {
   try {
     const { MONGODB_REMOTE_URI, MONGODB_LOCAL_URI } = process.env
-    const connect =await mongoose.connect(MONGODB_REMOTE_URI)
+    const connect = await mongoose.connect(MONGODB_REMOTE_URI);
     console.log(`connected to mongodb and using ${connect.connection.name} database on port ${connect.connection.port}`)
   } catch (error) {
     console.log("error: ",error)
